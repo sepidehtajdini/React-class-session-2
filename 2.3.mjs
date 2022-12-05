a9b307d2c094d00fe2c83057d99859d585a4ec9a
Sepideh <108736695+sepidehtajdini@users.noreply.github.com>
2022-12-05 15:10:36 +0330

    Update 2.3.mjs

--- 2.3.mjs
+++ 2.3.mjs
@@ -1,6 +1,4 @@
-import power from './my_modules.mjs';
-import square from "./my_modules.mjs";
+import {square, power} from "./my_modules.mjs";
 
-let number =prompt("Please enter the number");
-
+let number = prompt("Please enter the number");
 alert(`Square root:${square(number)} and Power:${power(number)}`);
