29d079ee18dc10d9599b95ffcb0e50f219ddb813
Sepideh <108736695+sepidehtajdini@users.noreply.github.com>
2022-12-05 14:30:09 +0330

    Update 2.2.js

--- 2.2.js
+++ 2.2.js
@@ -2,14 +2,14 @@ import axios from 'axios';
 import chalk from "chalk";
 
 const id = process.argv[2];
-
-const title = axios.get(`https://jsonplaceholder.typicode.com/todos/${id}`)
-    .then(function (json) { return json.data.title });
-
-
-const promise = axios.get(`https://jsonplaceholder.typicode.com/todos/${id}`)
+axios.get(`https://jsonplaceholder.typicode.com/todos/${id}`)
     .then(function (json) {
+        const title = json.data.title
         const comp = json.data.completed;
-        if (comp === true) { console.log(chalk.green(title)) }
-        else { console.log(chalk.red(title)) }
+
+        if (comp === true) {
+            console.log(chalk.green(title))
+        } else {
+            console.log(chalk.red(title))
+        }
     });
