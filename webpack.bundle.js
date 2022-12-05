4511619338fa76ff0a2a0c3eae320a78ce6d4402
Sepideh <108736695+sepidehtajdini@users.noreply.github.com>
2022-12-05 15:09:20 +0330

    Delete webpack.bundle.js

--- webpack.bundle.js
+++
@@ -1,1 +0,0 @@
-(()=>{"use strict";const e={power:function(e){return e**2},square:function(e){return Math.sqrt(e)}};let r=prompt("Please enter the number");alert(`Square root:${e(r)} and Power:${e(r)}`)})();
